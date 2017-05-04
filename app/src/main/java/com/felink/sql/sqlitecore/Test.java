ed7b359556b9f40f36b4c5a6378af60af7f3c747
linbin91 <774520452@qq.com>
2017-05-04 20:58:18 +0800

    m = 1

---
+++ app/src/main/java/com/felink/sql/sqlitecore/Test.java
@@ -0,0 +1,17 @@
+package com.felink.sql.sqlitecore;
+
+/**
+ * <pre>
+ *     author : linbin
+ *     e-mail : 18959298002@163.com
+ *     time   : 2017/05/04
+ *     desc   :
+ * </pre>
+ *
+ *
+ * */
+
+public class Test {
+
+    private int m = 1;
+}
